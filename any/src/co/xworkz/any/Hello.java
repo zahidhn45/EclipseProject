85067b19e9c78668e76cac18eea2d01499a1e4ec
Zahid <mdzahidhussain.xworkz@gmail.com>
2021-12-03 10:35:27 +0530

    2nd commit

--- any/src/co/xworkz/any/Hello.java
+++ any/src/co/xworkz/any/Hello.java
@@ -3,6 +3,6 @@ package co.xworkz.any;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World" + "Hello world");
 	}
 }
